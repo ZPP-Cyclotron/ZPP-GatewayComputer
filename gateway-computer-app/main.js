5d9d73aba768f5df48e6dad68d27f854c9d78620
Jan Roguwski <Jroguwski@gmail.com>
2023-12-02 16:45:44 +0100

    create templates directory  for frontend files, add css directory

--- gateway-computer-app/main.js
+++ gateway-computer-app/main.js
@@ -1,1 +1,14 @@
-console.log('main.js loaded');
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('templates/index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
