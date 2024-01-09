7b3a76e962dd54cdc90ad7bef31a7e4ab5ae48fb
Jan Roguwski <Jroguwski@gmail.com>
2024-01-09 23:58:36 +0100

    implement first version of communication API, using modbus library

--- gateway-computer-app/preload.js
+++ gateway-computer-app/preload.js
@@ -1,5 +1,16 @@
-const { contextBridge, ipcRenderer } = require('electron/renderer')
+const { contextBridge, ipcRenderer } = require("electron/renderer");
 
-contextBridge.exposeInMainWorld('electronAPI', {
-  otworzPlikKonfiguracyjny: () => ipcRenderer.invoke('dialog:otworzPlikKonfiguracyjny')
-})
+contextBridge.exposeInMainWorld("electronAPI", {
+  otworzPlikKonfiguracyjny: () =>
+    ipcRenderer.invoke("dialog:otworzPlikKonfiguracyjny"),
+  set_polarity: (new_val, supp_name) =>
+    ipcRenderer.invoke("dialog:set_polarity", new_val, supp_name),
+  set_current: (new_val, supp_name) =>
+    ipcRenderer.invoke("dialog:set_current", new_val, supp_name),
+  turn_on: (supp_name) => ipcRenderer.invoke("dialog:turn_on", supp_name),
+  turn_off: (supp_name) => ipcRenderer.invoke("dialog:turn_off", supp_name),
+  reset: (supp_name) => ipcRenderer.invoke("dialog:reset", supp_name),
+  // set control mode to manual or remote:
+  set_control_mode: (new_val) =>
+    ipcRenderer.invoke("dialog:set_control_mode", new_val),
+});
